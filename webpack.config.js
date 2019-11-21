d334d3b4ea210c2c9ff6e5a22acfb09d69846d54
Yuluncong <2722946251@qq.com>
2019-11-21 16:48:51 +0800

    :rocket:模块热替换

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
   // entry: './src/index.js',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    // app: './src/index.js',
+    // print: './src/print.js',
+    app: './src/index.js'
   },
   output: {
     // filename: 'bundle.js',
@@ -16,47 +18,50 @@ module.exports = {
   },
   devtool: 'inline-sourece-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    })
-  ]
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/,
-  //       use: [
-  //         'style-loader',
-  //         'css-loader'
-  //       ]
-  //     },
-  //     {
-  //       test: /\.(png|svg|jpg|gif)$/,
-  //       use: [
-  //         'file-loader'
-  //       ]
-  //     },
-  //     {
-  //       test: /\.(woff|woff2|eot|ttf|otf)$/,
-  //       use: [
-  //         'file-loader'
-  //       ]
-  //     },
-  //     {
-  //       test: /\.(csv|tsv)$/,
-  //       usr: [
-  //         'csv-loader'
-  //       ]
-  //     },
-  //     {
-  //       test: /\.xml$/,
-  //       usr: [
-  //         'csv-loader'
-  //       ]
-  //     }
-  //   ]
-  // }
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      // {
+      //   test: /\.(png|svg|jpg|gif)$/,
+      //   use: [
+      //     'file-loader'
+      //   ]
+      // },
+      // {
+      //   test: /\.(woff|woff2|eot|ttf|otf)$/,
+      //   use: [
+      //     'file-loader'
+      //   ]
+      // },
+      // {
+      //   test: /\.(csv|tsv)$/,
+      //   usr: [
+      //     'csv-loader'
+      //   ]
+      // },
+      // {
+      //   test: /\.xml$/,
+      //   usr: [
+      //     'csv-loader'
+      //   ]
+      // }
+    ]
+  }
 }
